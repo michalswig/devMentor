5767df12d6dee662076fcfcca2daee4be7951f12
michalswig <mike@data.pl>
2025-01-18 17:56:30 +0100

    project init

---
+++ src/main/java/com/mike/taskmanagement/TaskManagerApplication.java
@@ -0,0 +1,19 @@
+package com.mike.taskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableCaching
+@EnableAsync
+@EnableScheduling
+public class TaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerApplication.class, args);
+	}
+
+}
